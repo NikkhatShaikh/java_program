7d2d42c211f23e9376682b4155023938a4be0eef
Nikkhat <nikkhat.shaikh@caprium.com>
2024-06-27 17:29:41 +0530

    swap number

--- src/interviewPrograms/SwapNumber.java
+++ src/interviewPrograms/SwapNumber.java
@@ -5,15 +5,26 @@ public class SwapNumber {
 
         int a= 10, b=20, c=0;
         System.out.println("Before Swapping");
-        System.out.println("a= "+a+" b="+b);
-        b=a;    // b= 10
-        a=a+b; // 10+10 = 220
-//       c=a;
-//       a=b;
-//       b=c;
+        System.out.println("a="+a+" b="+b);
+//       b=a;       // b= 10
+//       a=a+b;    // 10+10 = 20
+
+        b=a;    // 10
+        a=b+b;  //20
+
+//       c=a;     //10
+//       a=b;    //20
+//       b=c;   //10
+
+//        a=a+b;   //10+20  30
+//        b=a-b;  //30-20   10
+//        a=a-b; //30-10   20
+
+
+
 
         System.out.println("after Swapping");
-        System.out.println("a= "+a+" b="+b);
+        System.out.println("a="+a+" b="+b);
 
 
 
