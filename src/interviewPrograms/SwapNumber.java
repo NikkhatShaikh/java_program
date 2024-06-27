2211e70999ad9709508832ef9866152fb4e65415
Nikkhat <nikkhat.shaikh@caprium.com>
2024-06-27 17:03:04 +0530

    swap number

---
+++ src/interviewPrograms/SwapNumber.java
@@ -0,0 +1,19 @@
+package interviewPrograms;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+
+        int a= 10, b=20;
+        System.out.println("Before Swapping");
+        System.out.println("a= "+a+" b="+b);
+        b=a;    // b= 10
+        a=a+b; // 10+10 = 220
+
+        System.out.println("after Swapping");
+        System.out.println("a= "+a+" b="+b);
+
+
+
+
+    }
+}
