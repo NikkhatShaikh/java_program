dec47e9b5da725913af99552b7c939884c9c65be
Nikkhat <nikkhat.shaikh@caprium.com>
2024-07-17 17:00:33 +0530

    increment /decrement oprators

---
+++ src/interviewPrograms/Oprtaors.java
@@ -0,0 +1,17 @@
+package interviewPrograms;
+
+public class Oprtaors {
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+        System.out.println( (a++)+1);
+        // post increment-value of the variable is used first, and then it is increased by one.
+        System.out.println( (++b)+1);
+        // pre increment-value of the variable is increased first and incremented by one then used.
+
+        System.out.println("------------------------------");
+        int x=5,y=5;
+        System.out.println((x--)-1);
+        System.out.println((--y)-1);
+    }
+}
