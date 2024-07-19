ef07d010cee9b7a621bb9da5e96c788f69040d4d
Nikkhat <nikkhat.shaikh@caprium.com>
2024-07-19 10:15:42 +0530

    operators

--- src/interviewPrograms/Oprators.java
+++ src/interviewPrograms/Oprators.java
@@ -4,9 +4,9 @@ public class Oprators {
     public static void main(String[] args) {
         int a=10;
         int b=10;
-        System.out.println( (a++)+1);
+        System.out.println( (a++)+1);  // 10+1=11 - value of a=10 will used first
         // post increment-value of the variable is used first, and then it is increased by one.
-        System.out.println( (++b)+1);
+        System.out.println( (++b)+1); //1+10+1=12
         // pre increment-value of the variable is increased first and incremented by one then used.
 
         System.out.println("------------------------------");
