1c5bb6c016cbf3cc854765b22e910111e70d10d8
shaikh nikkhat <nikkhat.shaikh@caprium.com>
2024-10-24 10:28:25 +0530

    infinite for  loop

---
+++ src/interviewPrograms/InfiniteLoop.java
@@ -0,0 +1,9 @@
+package interviewPrograms;
+
+public class InfiniteLoop {
+    public static void main(String[] args) {
+        for (int i = 0;i>=0 ; i++) {
+            System.out.println("This will run forever!");
+        }
+    }
+}
