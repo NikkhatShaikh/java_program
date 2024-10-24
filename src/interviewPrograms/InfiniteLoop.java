5fff32c9fae61f9e2f2db47a40ffad778ee897f4
shaikh nikkhat <nikkhat.shaikh@caprium.com>
2024-10-24 10:51:12 +0530

    ifinite for loop in differnt type

--- src/interviewPrograms/InfiniteLoop.java
+++ src/interviewPrograms/InfiniteLoop.java
@@ -2,8 +2,16 @@ package interviewPrograms;
 
 public class InfiniteLoop {
     public static void main(String[] args) {
-        for (int i = 0;i>=0 ; i++) {
-            System.out.println("This will run forever!");
+//        for (int i =0;i>=0 ; i++) {
+//            System.out.println("This will run forever!");
+//        }
+
+//        for (int i = 0;i<=0 ; i--) {
+//            System.out.println("This will run forever!");
+//        }
+
+        for(int i=0 ;;i++){
+            System.out.println("Nikkhat");
         }
     }
 }
